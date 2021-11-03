613dbdf827f31a6654eadf3e8970480c2e7b16af
halley <demeha@ssfs.org>
2021-11-03 14:30:50 -0400

    started unit 6 assignment

---
+++ assignment_six.py
@@ -0,0 +1,22 @@
+# Halley Deme
+# Date Last Edited: 11/03/2021
+# This program simulates multiple classes of 23 people to check for duplicate birthdays.
+# This is a model of the birthday paradox.
+import random
+def birthdays():
+    list_one = []
+    for x in range(23):
+        r=random.randint(1,365)
+        list_one.append(r)
+    print(list_one)
+
+def find_duplicate():
+    for x in range(22):
+        list[x]
+        for y in range(x+1,23):
+            list[y]
+    if list[x]== list[y]:
+        return true
+
+
+
